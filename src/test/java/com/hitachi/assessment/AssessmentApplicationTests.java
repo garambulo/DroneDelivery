d53e587e458569149277429ab80e54e3a7f2c7d1
garambulo <genrev_arambulo009@yahoo.com>
2025-04-06 14:02:15 +0800

    added initial files from spring initializr

---
+++ src/test/java/com/hitachi/assessment/AssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hitachi.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
